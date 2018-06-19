642529fc7a0a151b60b94a47edd755856bec4191
Minh Nguyen <gordonnguyen3796@gmail.com>
2018-06-19 16:01:00 +0700

    new infer

--- eval.py
+++ eval.py
@@ -1,11 +1,74 @@
 from boxes import compare
 import numpy as np
 import json
-from os import listdir
+from os import listdir, mkdir
+from os.path import join, exists
+from shutil import copy
 
-val_dir = 'validation_name_boxes'
+pred_dir = 'validation_name_boxes'
 gt_dir = '../processed/matched_json'
+cropped_dir = '../logs-FCN-OCR/model.ckpt-1/located_words/'
+full_img_with_boxes_dir = '../processed/matched_img/'
+wrong_img_correct_dir = 'wrong_images_correct'
+wrongly_cropped_correct_dir = 'wrongly_cropped_correct'
+wrong_img_recall_dir = 'wrong_images_recall'
+wrongly_cropped_recall_dir = 'wrongly_cropped_recall'
 
-files = listdir(val_dir)
+files = listdir(pred_dir)
+count_correct = 0
+count_recall = 0
+
 for file in files:
-    gt_data = 
+    with open(join(gt_dir, file)) as gt_file:
+        gt_data = json.load(gt_file)
+    json_gt_name_boxes = gt_data['name_boxes']
+    fullname_box = gt_data['full_name_boxes']
+    gt_name_boxes = []
+    for json_gt_name_box in json_gt_name_boxes:
+        gt_name_box = [0, 0, 0, 0]
+        gt_name_box[0] = json_gt_name_box[0] - fullname_box[0]
+        
+        if gt_name_box[0] < 0:
+            gt_name_box[0] = 0
+
+        gt_name_box[1] = json_gt_name_box[1] - fullname_box[1]
+        if gt_name_box[1] < 0:
+            gt_name_box[1] = 0
+
+        gt_name_box[2] = json_gt_name_box[2] - fullname_box[0]
+        if gt_name_box[2] > fullname_box[2] - fullname_box[0]:
+            gt_name_box[2] = fullname_box[2] - fullname_box[0]
+
+        gt_name_box[3] = json_gt_name_box[5] - fullname_box[1]
+        if gt_name_box[3] > fullname_box[3] - fullname_box[1]:
+            gt_name_box[3] = fullname_box[3] - fullname_box[1]
+        gt_name_boxes.append(gt_name_box)
+
+    with open(join(pred_dir, file)) as pred_file:
+        pred_data = json.load(pred_file)
+        pred_name_boxes = pred_data['name_boxes']
+        correct, recall, hits = compare(pred_name_boxes, gt_name_boxes)
+        
+        if correct == True:
+            count_correct += 1
+        else:
+            if not exists(wrong_img_correct_dir):
+                mkdir(wrong_img_correct_dir)
+            if not exists(wrongly_cropped_correct_dir):
+                mkdir(wrongly_cropped_correct_dir)
+            img_name = file.replace('json', 'png')
+            copy(join(full_img_with_boxes_dir, img_name), join(wrong_img_correct_dir, img_name))
+            copy(join(cropped_dir, img_name), join(wrongly_cropped_correct_dir, img_name))
+        if recall == True:
+            count_recall += 1
+        else:
+            if not exists(wrong_img_recall_dir):
+                mkdir(wrong_img_recall_dir)
+            if not exists(wrongly_cropped_recall_dir):
+                mkdir(wrongly_cropped_recall_dir)
+            img_name = file.replace('json', 'png')
+            copy(join(full_img_with_boxes_dir, img_name), join(wrong_img_recall_dir, img_name))
+            copy(join(cropped_dir, img_name), join(wrongly_cropped_recall_dir, img_name))
+    
+print("Correct:", count_correct / len(files))
+print("Recall:", count_recall / len(files))
--- infer_mask.py
+++ infer_mask.py
@@ -6,7 +6,7 @@ from sys import argv
 from batch_eval_top import create_patch_batch_list, batch_logits_map_inference
 import numpy as np
 from cv2 import imread, imwrite, threshold, findContours, boundingRect, rectangle, RETR_EXTERNAL, COLOR_GRAY2RGB
-from os.path import exists, join, splitext
+from os.path import exists, join
 import json
 
 IMAGE_SIZE = 32
@@ -14,6 +14,75 @@ CHOPPING_RATIO = 0.5
 data_dir = '../FCN_OCR_dataset'
 annotation_dir = 'annotations'
 input_dir = 'input_img'
+words_dir = 'located_words'
+pred_masks_dir = 'pred_masks'
+pred_boxed_masks_dir = 'pred_boxed_masks'
+num_matches = 0
+num_pixels = 0
+
+def infer_img(file, model_output_dir=argv[1]):
+    global num_matches
+    global num_pixels
+    if not exists(model_output_dir):
+        mkdir(model_output_dir)
+
+    gt_annotation_map = np.array(imread(join(data_dir, annotation_dir, file), -1), dtype=np.uint8)
+    input_img = imread(join(data_dir, input_dir, file))
+
+    input_batch_list, coordinate_batch_list, height, width = create_patch_batch_list(filename=file, batch_size=128)
+    logits_map = batch_logits_map_inference(input_tensor, logits, keep_probability, sess, is_training, input_batch_list, coordinate_batch_list, height, width)
+
+    # Inferring
+    pred_annotation_map = np.array(np.argmax(logits_map, axis=2), dtype=np.uint8)
+    _, thresh = threshold(pred_annotation_map, 0, 255, 0)
+    _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
+    for cnt in contours:
+        x,y,w,h = boundingRect(cnt)
+        for i in range(w):
+            count_white = 0
+            for j in range(h):
+                if thresh[y + j, x + i] == 255:
+                    count_white += 1
+            if count_white / h <= CHOPPING_RATIO:
+                for j in range(h):
+                    pred_annotation_map[y + j, x + i] = 0
+    if not exists(join(model_output_dir, pred_masks_dir)):
+        mkdir(join(model_output_dir, pred_masks_dir))
+    _, thresh = threshold(pred_annotation_map, 0, 255, 0)
+    imwrite(join(model_output_dir, pred_masks_dir, file), thresh)
+
+    boxes_map = np.zeros_like(pred_annotation_map, dtype=np.uint8)
+    _, thresh = threshold(pred_annotation_map, 0, 255, 0)
+    _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
+    json_name = file.replace('png', 'json')
+    json_dict = {}
+    name_boxes = []
+    for cnt in contours:
+        x,y,w,h = boundingRect(cnt)
+        name_boxes.append([x, y, x + w, y + h])
+        for i in range(w):
+            for j in range(h):
+                boxes_map[y + j, x + i] = 1
+    _, thresh_boxes = threshold(boxes_map, 0, 255, 0)
+    if not exists(join(model_output_dir, pred_boxed_masks_dir)):
+        mkdir(join(model_output_dir, pred_boxed_masks_dir))
+    imwrite(join(model_output_dir, pred_boxed_masks_dir, file), thresh_boxes)
+    json_dict['name_boxes'] = name_boxes
+    print(json_dict)
+    with open(join('validation_name_boxes', json_name), 'w') as outfile:
+        json.dump(json_dict, outfile)
+
+    height = pred_annotation_map.shape[0]
+    width = pred_annotation_map.shape[1]
+    # _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
+    num_pixels += height * width
+    num_matches += np.sum(pred_annotation_map == gt_annotation_map)
+
+    print("Locating", file + '......')
+    output_words = np.transpose(np.transpose(input_img, (2, 0, 1)) * boxes_map, (1, 2, 0))
+    if not exists(join(model_output_dir, words_dir)):
+        mkdir(join(model_output_dir, words_dir))
+    imwrite(join(model_output_dir, words_dir, file), output_words)
 
 if __name__ == '__main__':
     environ["CUDA_VISIBLE_DEVICES"] = argv[2]
@@ -33,61 +102,8 @@ if __name__ == '__main__':
     files = listdir(join(data_dir, annotation_dir))
     validation_image = files[int(len(files) * 0.8):]
 
-    num_matches = 0
-    num_pixels = 0
     for file in validation_image:
-        gt_annotation_map = np.array(imread(join(data_dir, annotation_dir, file), -1), dtype=np.uint8)
-        input_img = imread(join(data_dir, input_dir, file))
-
-        input_batch_list, coordinate_batch_list, height, width = create_patch_batch_list(filename=file, batch_size=128)
-        logits_map = batch_logits_map_inference(input_tensor, logits, keep_probability, sess, is_training, input_batch_list, coordinate_batch_list, height, width)
-
-        # Inferring
-        pred_annotation_map = np.array(np.argmax(logits_map, axis=2), dtype=np.uint8)
-        _, thresh = threshold(pred_annotation_map, 0, 255, 0)
-        _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
-        for cnt in contours:
-            x,y,w,h = boundingRect(cnt)
-            for i in range(w):
-                count_white = 0
-                for j in range(h):
-                    if thresh[y + j, x + i] == 255:
-                        count_white += 1
-                if count_white / h <= CHOPPING_RATIO:
-                    for j in range(h):
-                        pred_annotation_map[y + j, x + i] = 0
-
-        boxes_map = np.zeros_like(pred_annotation_map, dtype=np.uint8)
-        _, thresh = threshold(pred_annotation_map, 0, 255, 0)
-        _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
-        json_name = splitext(file)[0] + '.json'
-        json_dict = {}
-        name_boxes = []
-        for cnt in contours:
-            x,y,w,h = boundingRect(cnt)
-            name_boxes.append([x, y, x + w, y + h])
-            for i in range(w):
-                for j in range(h):
-                    boxes_map[y + j, x + i] = 1
-        json_dict['name_boxes'] = name_boxes
-        print(json_dict)
-        with open(join('validation_name_boxes', json_name), 'w') as outfile:
-            json.dump(json_dict, outfile)
-
-        height = pred_annotation_map.shape[0]
-        width = pred_annotation_map.shape[1]
-        # _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
-        num_pixels += height * width
-        num_matches += np.sum(pred_annotation_map == gt_annotation_map)
-
-        print("Locating", file + '......')
-        if not exists(argv[1]):
-            mkdir(argv[1])
-        if not exists(join(argv[1], 'located_words')):
-            mkdir(join(argv[1], 'located_words'))
-        output_image = np.transpose(np.transpose(input_img, (2, 0, 1)) * boxes_map, (1, 2, 0))
-        imwrite(join(argv[1], 'located_words', file), output_image)
-
+        infer_img(file)
     # Print accuracy
     print("Validation accuracy:", num_matches / num_pixels)
 
