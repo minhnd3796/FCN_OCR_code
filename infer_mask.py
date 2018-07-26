b981cec15a279f3e82ab4d08c94ee332bb73f44c
Minh Nguyen <gordonnguyen3796@gmail.com>
2018-07-26 16:35:27 +0700

    Edit quangbt

--- infer_mask.py
+++ infer_mask.py
@@ -6,7 +6,7 @@ from sys import argv
 from batch_eval_top import create_patch_batch_list, batch_logits_map_inference
 import numpy as np
 from cv2 import imread, imwrite, threshold, findContours, boundingRect, rectangle, RETR_EXTERNAL, COLOR_GRAY2RGB, namedWindow, imshow, waitKey, destroyAllWindows, contourArea
-from os.path import exists, join
+from os.path import exists, join, basename
 import json
 
 IMAGE_SIZE = 32
@@ -23,15 +23,16 @@ num_pixels = 0
 pad = 0
 
 def infer_one_img(file):
-    input_img = imread(join(data_dir, input_dir, file))
+    input_img = imread(file)
     input_batch_list, coordinate_batch_list, height, width = create_patch_batch_list(filename=file, batch_size=128, data_dir=data_dir)
     logits_map = batch_logits_map_inference(input_tensor, logits, keep_probability, sess, is_training, input_batch_list, coordinate_batch_list, height, width)
     # Inferring
     pred_annotation_map = np.array(np.argmax(logits_map, axis=2), dtype=np.uint8)
+    imwrite('mask_1_' + basename(file), np.transpose(np.transpose(input_img, (2, 0, 1)) * pred_annotation_map, (1, 2, 0)))
     _, thresh = threshold(pred_annotation_map, 0, 255, 0)
     _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
     for cnt in contours:
-        x, y, w, h = boundingRect(cnt)
+        x,y,w,h = boundingRect(cnt)
         for i in range(w):
             count_white = 0
             for j in range(h):
@@ -40,21 +41,33 @@ def infer_one_img(file):
             if count_white / h <= CHOPPING_RATIO:
                 for j in range(h):
                     pred_annotation_map[y + j, x + i] = 0
+    _, thresh = threshold(pred_annotation_map, 0, 255, 0)
+    imwrite('mask_2_' + basename(file), np.transpose(np.transpose(input_img, (2, 0, 1)) * pred_annotation_map, (1, 2, 0)))
 
     boxes_map = np.zeros_like(pred_annotation_map, dtype=np.uint8)
     _, thresh = threshold(pred_annotation_map, 0, 255, 0)
     _, contours, _ = findContours(thresh, RETR_EXTERNAL, 2)
+    name_boxes = []
     for cnt in contours:
+        if contourArea(cnt) > 200:
         x,y,w,h = boundingRect(cnt)
+            input_height, _, _ = input_img.shape
+            # x = max(0, x - pad)
+            y = max(0, y - pad)
+            # end_x = min(x + w + pad, input_width)
+            end_x = x + w
+            end_y = min(y + h + pad, input_height)
+            # w = end_x - x
+            h = end_y - y
+            if w > h / 1.75:
+                name_boxes.append([x, y, end_x, end_y])
         for i in range(w):
             for j in range(h):
                 boxes_map[y + j, x + i] = 1
+    height = pred_annotation_map.shape[0]
+    width = pred_annotation_map.shape[1]
     output_words = np.transpose(np.transpose(input_img, (2, 0, 1)) * boxes_map, (1, 2, 0))
-    """ namedWindow('image', cv2.WINDOW_NORMAL)
-    imshow('image', output_words)
-    waitKey(0)
-    destroyAllWindows() """
-    imwrite('/home/minhnd/Desktop/hehe.png', output_words)
+    imwrite(basename(file), output_words)
 
 def infer_img(file, model_output_dir=argv[1]):
     global num_matches
@@ -148,11 +161,11 @@ if __name__ == '__main__':
     saver.restore(sess, argv[1])
     print(">> Restored:", argv[1])
 
-    files = listdir(join(data_dir, annotation_dir))
+    """ files = listdir(join(data_dir, annotation_dir))
     validation_image = files[int(len(files) * 0.8):]
     for file in validation_image:
         print("Locating", file)
         infer_img(file)
-    print("Validation accuracy:", num_matches / num_pixels)
+    print("Validation accuracy:", num_matches / num_pixels) """
 
-    # infer_one_img(argv[3])
+    infer_one_img(argv[3])
